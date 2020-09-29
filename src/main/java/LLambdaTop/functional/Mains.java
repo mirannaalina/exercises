fe1c26cb6f76c811a1b6b2499fd24a8a6e4fb741
mirannaalina <v_mxa@yahoo.com>
2020-09-29 16:51:47 +0300

    pred

--- src/main/java/LLambdaTop/functional/Mains.java
+++ src/main/java/LLambdaTop/functional/Mains.java
@@ -2,17 +2,20 @@ package LLambdaTop.functional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Mains {
 
     public static void main(String[] args) {
 
-        Employee john = new Employee("john",23);
-        Employee  sol = new Employee("sol",22);
-        Employee al = new Employee("al",66);
-        Employee cri = new Employee("cri",11);
+        Employee john = new Employee("john ss",23);
+        Employee  sol = new Employee("sol rrt",22);
+        Employee al = new Employee("al gg",66);
+        Employee cri = new Employee("cri ooo",11);
 
         List<Employee>  employeeList = new ArrayList<>();
 
@@ -28,11 +31,39 @@ public class Mains {
             }
         });
 
+        //predicate
+
+
         IntPredicate intp = i -> i>15;
         System.out.println(intp.test(10));
         int a=20;
         System.out.println(intp.test(a+5));
         System.out.println("----------------------------------");
+
+
+        //supplier
+
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = ()->random.nextInt(1000);
+        for(int i=0;i<10;i++){
+            System.out.println(random.nextInt(1000));
+            System.out.println(randomSupplier.get() );
+        }
+
+        System.out.println("-------------------------");
+        //get last name from list
+        Function<Employee, String> getLastName = (Employee employee)->{
+            return employee.getName().substring(employee.getName().indexOf(' ')+1);
+        };
+
+        String lastName = getLastName.apply(employeeList.get(1));
+        System.out.println(lastName);
+
+        Function<Employee, String> getFirstName = (Employee employee)-> {
+            return employee.getName().substring(0,employee.getName().indexOf(' '));
+        };
+
+
 
         //object in, nothing out
         //consumer
@@ -52,9 +83,18 @@ public class Mains {
                 System.out.println(employee.getName());
             }
         });
+
+        Random random1 = new Random();
+        for (Employee employee : employeeList){
+            if(random1.nextBoolean()){
+                System.out.println(getAName(getFirstName,employee));
+            }else{
+                System.out.println(getAName(getLastName,employee));
+            }
+        }
     }
 
-    private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition){
+    private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
         System.out.println("over 30 ");
 //        for(Employee employee :employees){
 //            if(ageCondition.test(employee)){
@@ -62,9 +102,13 @@ public class Mains {
 //           }
 //        }
         employees.forEach(employee -> {
-            if(employee.getAge()>30){
+            if (employee.getAge() > 30) {
                 System.out.println(employee.getName());
             }
         });
+
+    }
+    private static String getAName(Function<Employee, String> getName, Employee employee){
+        return "S";
     }
 }
