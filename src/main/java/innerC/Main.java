ac355d8920fdb84a99aaa88d78f83e29ace7b73f
mirannaalina <v_mxa@yahoo.com>
2020-10-14 21:03:32 +0300

    interChal9

--- src/main/java/innerC/Main.java
+++ src/main/java/innerC/Main.java
@@ -1,13 +1,31 @@
 package innerC;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        GearBox mcLAren=  new GearBox(6);
-        GearBox.Gear first = mcLAren.new Gear(1,2.3);
-       //GearBox.Gear second = new mcLAren.Gear()
-        System.out.println(first.driveSpeed(1000));
+    private static Scanner sc = new Scanner(System.in);
+    private static Button bt = new Button("Print");
 
-        mcLAren.changeGear(1);
+    public static void main(String[] args) {
+
+
+        class ClickListener{
+            public ClickListener(){
+                System.out.println("I've been attached");
+            }
+
+
+        }
+
+
+
+
+//        GearBox mcLAren=  new GearBox(6);
+//        GearBox.Gear first = mcLAren.new Gear(1,2.3);
+//       //GearBox.Gear second = new mcLAren.Gear()
+//        System.out.println(first.driveSpeed(1000));
+//
+//        mcLAren.changeGear(1);
     }
 }
